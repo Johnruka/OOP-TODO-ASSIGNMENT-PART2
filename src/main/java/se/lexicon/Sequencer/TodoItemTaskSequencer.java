756b52cae8e3628f9c315ce0526e9725c74daa42
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-08-30 11:57:56 +0200

    implemented

--- src/main/java/se/lexicon/Sequencer/TodoItemTaskSequencer.java
+++ src/main/java/se/lexicon/Sequencer/TodoItemTaskSequencer.java
@@ -1,4 +1,10 @@
 package se.lexicon.Sequencer;
 
 public class TodoItemTaskSequencer {
+
+    private static int sequencer = 0;
+
+    public static int nextId() {
+        return ++sequencer;
+    }
 }
